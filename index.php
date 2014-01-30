717be72e4f74f36fa6accea7965acfd032d6c9ba
Kamil Zoń <highlandmind@gmail.com>
2014-01-30 22:26:18 +0100

    Zmiana layoutu, dodana funckja wyświetlająca format daty

--- index.php
+++ index.php
@@ -3,54 +3,62 @@
 <html class="no-js" lang="en" >
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Foundation 5</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Foundation 5</title>
 
-  <!-- If you are using CSS version, only link these 2 files, you may add app.css to use for your overrides if you like. -->
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/foundation.css">
+	<!-- If you are using CSS version, only link these 2 files, you may add app.css to use for your overrides if you like. -->
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/foundation.css">
 
-  <!-- If you are using the gem version, you need this only -->
-  <link rel="stylesheet" href="css/app.css">
+	<!-- If you are using the gem version, you need this only -->
+	<link rel="stylesheet" href="css/app.css">
 
-  <script src="js/vendor/modernizr.js"></script>
+	<script src="js/vendor/modernizr.js"></script>
 
 </head>
 <body>
 
-  <!-- body content here -->
+	<!-- body content here -->
 
-  <div class="row">
-  	<div class="medium-6 columns panel radius" id="container">
-  		
-  	</div>
-  </div>
+	<div class="row">
 
-  <script src="js/vendor/jquery.js"></script>
-  <script src="js/foundation.min.js"></script>
-  <script>
-    $(document).foundation();
-    $(document).ready(function(){
-    	$.ajax({
-    		url: 'getdata.php',
-    		dataType: 'json'
-    	}).done(function(r){
-<<<<<<< HEAD
-    		//$('#container').html(r);
-    		//var obj= $.parseJSON(r);
-    		var emptyDate = new Date();
-    		var date = new Date(r.sys.sunrise * 1000 + emptyDate.getTimezoneOffset() * 60000);
-    		$('#container').html('<p>' + r.name + ', ' + r.sys.country );
-    		$('#container').append( date.toUTCString()+ '</p>');
-    		$('#container').append('<img src="http://openweathermap.org/img/w/' + r.weather[0].icon + '.png" />');
-    		$('#container').append(r.weather[0].description);
-    		$('#container').append('<p> Temperatóra: ' + r.main.temp + ' &deg;C </p>');
-=======
-    		$('#container').html('<img src="http://openweathermap.org/img/w/' + r.weather[0].icon + '.png" />');
->>>>>>> f330d478fc4caf038ec6f10a139df785f4074a6f
-    	});
-    });
-  </script>
+		<div class="medium-6 columns">	
+			<ul class="medium-block-grid-2" id="container">
+			</ul>
+		</div>
+
+		<div class="medium-6 columns">
+		</div>
+	</div>
+
+	<script src="js/vendor/jquery.js"></script>
+	<script src="js/foundation.min.js"></script>
+	<script>
+	$(document).foundation();
+	$(document).ready(function(){
+		$.ajax({
+			url: 'getdata.php',
+			dataType: 'json'
+		}).done(function(r){
+			$('#container').append('<li class="panel radius">' + formatData(r) + '</li>');
+		});
+	});
+
+	function formatData(r) {
+		var out = '';
+
+		var emptyDate = new Date();
+		var date = new Date(r.sys.sunrise * 1000 + emptyDate.getTimezoneOffset() * 60000);
+
+		out += '<p>' + r.name + ', ' + r.sys.country;
+		out +=  date.toUTCString()+ '</p>';
+		out += '<img src="http://openweathermap.org/img/w/' + r.weather[0].icon + '.png" />';
+		out += r.weather[0].description;
+		out += '<p> Temperatóra: ' + r.main.temp + ' &deg;C </p>';
+
+		return out;
+	}
+	</script>
 </body>
 </html>
